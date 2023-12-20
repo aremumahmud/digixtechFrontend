66596398df6528337ee6af564bae9b3c4df79957
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2023-12-20 09:02:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+// import HomeLoader from './components/homeLoader'
+import Onboard from './components/onboard'
+
+function App() {
+    return (
+<div>
+    {/* <HomeLoader /> */}
+    <Onboard />
+</div>
+        
+    );
+}
+
+export default App;
